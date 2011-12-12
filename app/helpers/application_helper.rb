3719054c963dc02fd67115d9e6b373880e9bf6fc
Doug Van Wie <doug@dougvanwie.com>
2011-12-11 19:15:48 -0500

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Return a title depending on the page
+	def title
+		base_title = "Ruby on Rails Tutorial"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
 end
